07e01056d1955020b6da79960142a9387624a671
Mohammad Uzair Ansari <146664651+uzair-ansar@users.noreply.github.com>
2024-01-26 16:39:20 +0530

    Update number_guessing.py

--- number_guessing/number_guessing.py
+++ number_guessing/number_guessing.py
@@ -1,1 +1,30 @@
+import random
 
+lower_num, higher_num = 1, 10
+random_num = random.randint(lower_num, higher_num)
+# print(random_num)
+print(f"Guess the number in the range of {lower_num} and {higher_num}.")
+
+limit = 10-1
+print(f"Total limit left {limit + 1}")
+game_active = True
+while game_active:
+    try:
+        user_guess = int(input("Guess: "))
+    except ValueError as e:
+        print("Please enter a valid number.")
+
+    if limit == 0:
+        print("Limit reached! restart the game again")
+        game_active = False
+    
+    if user_guess > random_num:
+        print("The number is lower")
+        limit -= 1
+    elif user_guess < random_num:
+        print("The number is higher")
+        limit -= 1
+    
+    else:
+        print("You guessed it!")
+        break
