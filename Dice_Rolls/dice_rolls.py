0f24b0f2aa705c00e338a7035686cb05c377c357
Mohammad Uzair Ansari <146664651+uzair-ansar@users.noreply.github.com>
2024-01-30 20:36:02 +0530

    creating dice roll project dice_rolls.py

---
+++ Dice_Rolls/dice_rolls.py
@@ -0,0 +1,32 @@
+import random
+
+def roll_dice(amount):
+    if amount <= 0:
+        raise ValueError
+    
+    rolls = []
+    for i in range(amount):
+        random_roll = random.randint(1, 6)
+        rolls.append(random_roll)
+
+    return rolls
+
+def main():
+    while True:
+        try:
+            user_input = input("How many dice would you like to roll? ")
+
+            if user_input.lower() == "exit":
+                print("Thanks for playing!")
+                break
+            result = roll_dice(int(user_input))
+            print(*result, sep=", ")
+            sum_result = sum(result)
+            print(f"Sum of the dice rolls: {sum_result}")
+            
+
+        except ValueError:
+            print("Please provide a valid input")
+
+if __name__ == '__main__':
+    main()
